af2882f163eba24126b5f4f6d3c6178bbf77b0f3
pavel_gavrilichev <Pavel.Gavrilichev@waveaccess.ru>
2022-05-26 23:21:35 +0300

    add admin actor component

--- src/app/components/administration-actor/administration-actor.component.ts
+++ src/app/components/administration-actor/administration-actor.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { ActorDetail } from '../../models/actorDetail';
+import { MovieService } from '../../services/movie.service';
 
 @Component({
   selector: 'app-administration-actor',
   templateUrl: './administration-actor.component.html',
-  styleUrls: ['./administration-actor.component.scss']
+  styleUrls: ['./administration-actor.component.scss',
+              '../administration/administration.component.scss']
 })
 export class AdministrationActorComponent implements OnInit {
 
-  constructor() { }
+  actors: ActorDetail[];
+  displayedColumns: string[] = ['id', 'name', 'lastName', 'country', 'update', 'remove'];
+
+  constructor(private movieService: MovieService) { }
 
   ngOnInit(): void {
+    this.getActors();
   }
 
+
+  getActors(){
+    this.movieService.getActorsDetails().subscribe(res=>{
+      this.actors = res;
+    })
+  }
 }
