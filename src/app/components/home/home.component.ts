18a96bcc15bf5a3d04fe2c6c60dbd56f2a955e8f
pavel_gavrilichev <Pavel.Gavrilichev@waveaccess.ru>
2022-06-04 18:17:21 +0300

    add pagination and sorting on home page

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,13 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { AccountService } from 'src/app/services/account.service';
 import { MovieService } from '../../services/movie.service';
-import { Page } from '../../common/table.types';
+import { Page, SortingEnum } from '../../common/table.types';
 import { MovieItem } from '../../models/movie/movieItem';
-import { FormArray, FormControl, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup } from '@angular/forms';
 import { CountryRef } from '../../models/countryRef';
 import { Genre } from 'src/app/models/genre/genre';
 import { MovieFilterParameters } from '../../models/movie/movieFilterParameters';
-import { Location } from '@angular/common'
 
 
 @Component({
@@ -26,9 +25,18 @@ export class HomeComponent implements OnInit {
   selectedCountry: string;
   movieFilter: MovieFilterParameters = null;
 
+  pageNumbers: number[];
+  pageSize: number = 2;
+  activePage = 1;
+  totalPages: number;
+
+  pageSizeOptions: number[] = [2, 4, 6];
+  sortColumns: Object = { "uploaddate": "Дате загрузки", "year": "Году выпуска", "name": "Названию", "rating": "Рейтигу" };
+  sortOrder = SortingEnum;
+  activeSortColumn: string;
+
   constructor(private auth: AccountService,
-              private movieService: MovieService,
-              private location: Location){ }
+    private movieService: MovieService) { }
 
   ngOnInit(): void {
     this.getAllMovies();
@@ -39,43 +47,175 @@ export class HomeComponent implements OnInit {
       yearTo: new FormControl(null),
       countryId: new FormControl(null),
       name: new FormControl(null),
-      genres: new FormControl(null)
+      genres: new FormControl(null),
+      sortColumn: new FormControl(null)
     });
   }
 
-  public get isLoggedIn(): boolean{
+  public get isLoggedIn(): boolean {
     return this.auth.isAunthenticated();
   }
 
- getAllMovies(){
-    this.movieService.getMovies(this.movieFilter).subscribe(res=>{
+  getAllMovies() {
+    this.movieFilter = new MovieFilterParameters(this.pageSize, 0, "uploaddate", this.sortOrder.DESC);
+    this.movieService.getMovies(this.movieFilter).subscribe(res => {
       this.movies = res['items'];
       this.total = res['total'];
+      this.filterForm.patchValue({ sortColumn: "uploaddate" });
+      this.activeSortColumn = "uploaddate";
+      this.totalPages = Math.trunc(this.total / this.pageSize) + 1;
+
+      if (this.totalPages < 7) {
+        this.pageNumbers = Array.from({ length: this.totalPages }, (_, i) => i + 1);
+      } else {
+        this.pageNumbers = [1, 2, 3, 4, 5, 6]
+      }
     });
   }
 
-  getCountriesRef(){
-    this.movieService.getCounties().subscribe(res=>{
+  getCountriesRef() {
+    this.movieService.getCounties().subscribe(res => {
       this.countries = res;
     });
   }
 
-  getAllGenres(){
-    this.movieService.getGenres().subscribe(res=>{
+  getAllGenres() {
+    this.movieService.getGenres().subscribe(res => {
       this.genresList = res;
     });
   }
 
-  applyFilters(){
+  applyFilters() {
     this.movieFilter.countryId = this.filterForm.value.countryId ? Number(this.filterForm.value.countryId) : null;
     this.movieFilter.yearFrom = this.filterForm.value.yearFrom ? Number(this.filterForm.value.yearFrom) : null;
     this.movieFilter.yearTo = this.filterForm.value.yearTo ? Number(this.filterForm.value.yearTo) : null;
     this.movieFilter.name = this.filterForm.value.name;
     this.movieFilter.genres = this.filterForm.value.genres;
-    
-    this.movieService.getMovies(this.movieFilter).subscribe(res=>{
+
+    this.movieService.getMovies(this.movieFilter).subscribe(res => {
       this.movies = res['items'];
       this.total = res['total'];
+      this.totalPages = Math.trunc(this.total / this.pageSize) + 1;
+
+      if (this.totalPages < 7) {
+        this.pageNumbers = Array.from({ length: this.totalPages }, (_, i) => i + 1);
+      } else {
+        this.pageNumbers = [1, 2, 3, 4, 5, 6]
+      }
+    });
+  }
+
+  removeFilters() {
+    this.pageSize = this.pageSizeOptions[0];
+    this.getAllMovies();
+    this.filterForm.reset();
+  }
+
+  pageChangeClick(page: number) {
+    if (this.pageNumbers.indexOf(page) == 0) {
+      if (this.pageNumbers[0] - 2 > 0) {
+        this.pageNumbers = this.pageNumbers.map(i => i - 2);
+      } else if (this.pageNumbers[0] - 1 > 0) {
+        this.pageNumbers = this.pageNumbers.map(i => i - 1);
+      }
+    } else if (this.pageNumbers.indexOf(page) == 5) {
+      if (this.pageNumbers[5] + 2 <= this.totalPages) {
+        this.pageNumbers = this.pageNumbers.map(i => i + 2);
+      } else if (this.pageNumbers[5] + 1 <= this.totalPages) {
+        this.pageNumbers = this.pageNumbers.map(i => i + 1);
+      }
+    }
+
+    this.activePage = page;
+    this.movieFilter.offset = this.activePage - 1;
+    this.movieService.getMovies(this.movieFilter).subscribe(res => {
+      this.movies = res['items'];
+    });
+  }
+
+  toFirstPage() {
+    this.activePage = 1;
+    if (this.totalPages < 7) {
+      this.pageNumbers = Array.from({ length: this.totalPages }, (_, i) => i + 1);
+    } else {
+      this.pageNumbers = [1, 2, 3, 4, 5, 6]
+    }
+
+    this.movieFilter.offset = this.activePage - 1;
+    this.movieService.getMovies(this.movieFilter).subscribe(res => {
+      this.movies = res['items'];
+    });
+  }
+
+  toPreviousPage() {
+    if (this.activePage > 1) {
+      if (this.pageNumbers[0] > 1 && this.pageNumbers.indexOf(this.activePage) == 0) {
+        this.pageNumbers = this.pageNumbers.map(i => i - 1);
+      }
+      this.activePage = this.activePage - 1;
+    }
+
+    this.movieFilter.offset = this.activePage - 1;
+    this.movieService.getMovies(this.movieFilter).subscribe(res => {
+      this.movies = res['items'];
+    });
+  }
+
+  toLastPage() {
+    this.activePage = this.totalPages;
+    if (this.totalPages < 7) {
+      this.pageNumbers = Array.from({ length: this.totalPages }, (_, i) => i + 1);
+    } else {
+      this.pageNumbers = [this.totalPages - 5, this.totalPages - 4, this.totalPages - 3, this.totalPages - 2, this.totalPages - 1, this.totalPages]
+    }
+
+    this.movieFilter.offset = this.activePage - 1;
+    this.movieService.getMovies(this.movieFilter).subscribe(res => {
+      this.movies = res['items'];
+    });
+  }
+
+  toNextPage() {
+    if (this.activePage < this.totalPages) {
+      if (this.pageNumbers[5] < this.totalPages && this.pageNumbers.indexOf(this.activePage) == 5) {
+        this.pageNumbers = this.pageNumbers.map(i => i + 1);
+      }
+      this.activePage = this.activePage + 1;
+    }
+
+    this.movieFilter.offset = this.activePage - 1;
+    this.movieService.getMovies(this.movieFilter).subscribe(res => {
+      this.movies = res['items'];
+    });
+  }
+
+  selectPageSize() {
+    this.movieFilter.limit = this.pageSize;
+    this.activePage = 1;
+    this.movieFilter.offset = this.activePage - 1;
+    this.movieService.getMovies(this.movieFilter).subscribe(res => {
+      this.movies = res['items'];
+      this.totalPages = Math.trunc(this.total / this.pageSize) + 1;
+
+      if (this.totalPages < 7) {
+        this.pageNumbers = Array.from({ length: this.totalPages }, (_, i) => i + 1);
+      } else {
+        this.pageNumbers = [1, 2, 3, 4, 5, 6]
+      }
+    });
+  }
+
+  selectSortColumn(sortColumnName: string) {
+    if (this.activeSortColumn == sortColumnName) {
+      this.movieFilter.sortOrder = this.movieFilter.sortOrder == SortingEnum.DESC ? SortingEnum.ASC : SortingEnum.DESC;
+    } else {
+      this.activeSortColumn = sortColumnName;
+      this.movieFilter.sortOrder = SortingEnum.DESC;
+    }
+
+    this.movieFilter.sortColumn = sortColumnName;
+    this.movieService.getMovies(this.movieFilter).subscribe(res => {
+      this.movies = res['items'];
     });
   }
 }
--- src/app/models/movie/movieFilterParameters.ts
+++ src/app/models/movie/movieFilterParameters.ts
@@ -1,7 +1,10 @@
+import { SortingEnum } from '../../common/table.types';
 export class MovieFilterParameters{
     constructor(
         public limit: number,
         public offset: number,
+        public sortColumn?: string,
+        public sortOrder?: SortingEnum,
         public yearFrom?: number,
         public yearTo?: number,
         public name?: string,
